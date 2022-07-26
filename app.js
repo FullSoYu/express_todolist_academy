e197b2074ad7bdf5d66ed8262d5f7414e0399291
FullSoYu <hey6410@naver.com>
2022-07-26 18:48:03 +0900

    단건 조회

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// app.js
 import express from "express";
 import mysql from "mysql2/promise";
 
-const app = express();
-
-app.use(express.json());
-
-const port = 3000;
 const pool = mysql.createPool({
   host: "localhost",
   user: "sbsst",
@@ -17,38 +11,27 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-app.get("/todos/:id/:contentId", async (req, res) => {
-  // params 여러개 받기
-  const data = {
-    todos: {
-      id: req.params.id,
-      contentId: req.params.contentId,
-    },
-  };
-
-  const {
-    todos: { id, contentId },
-  } = data;
-
-  console.log("id", id);
-});
+const app = express();
+const port = 3000;
 
 app.get("/todos", async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
-
-  getData();
+  const [rows] = await pool.query(`
+  SELECT * 
+  FROM todo
+  ORDER BY id DESC
+  `);
   res.json(rows);
 });
 
-app.get("/todos/:id/", async (req, res) => {
-  //const id = req.params.id;
+app.get("/todos/:id", async (req, res) => {
   const { id } = req.params;
 
   const [rows] = await pool.query(
     `
-  SELECT *
-  FROM todo
-  WHERE id = ?
+
+    SELECT * 
+    FROM todo
+    WHERE id = ?
   `,
     [id]
   );
@@ -58,84 +41,7 @@ app.get("/todos/:id/", async (req, res) => {
     });
     return;
   }
-
   res.json(rows[0]);
-});
-
-app.patch("/todos/:id", async (req, res) => {
-  const { id } = req.params;
-  const { perform_date, content } = req.body;
-
-  const [rows] = await pool.query(
-    `
-    SELECT *
-    FROM todo
-    WHERE id = ?
-    `,
-    [id]
-  );
-
-  if (rows.length === 0) {
-    res.status(404).json({
-      msg: "not found",
-    });
-  }
-
-  if (!perform_date) {
-    res.status(400).json({
-      msg: "perform_date required",
-    });
-    return;
-  }
-
-  if (!content) {
-    res.status(400).json({
-      msg: "content required",
-    });
-    return;
-  }
-
-  const [rs] = await pool.query(
-    `
-    UPDATE todo
-    SET perform_date = ?,
-    content = ?
-    WHERE id = ?
-    `,
-    [perform_date, content, id]
-  );
-
-  res.json({
-    msg: `${id}번 할일이 수정되었습니다.`,
-  });
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const [[todoRow]] = await pool.query(
-    `
-    SELECT *
-    FROM todo
-    WHERE id = ?`,
-    [id]
-  );
-
-  if (todoRow === undefined) {
-    res.status(404).json({
-      msg: "not found",
-    });
-    return;
-  }
-
-  const [rs] = await pool.query(
-    `DELETE FROM todo
-    WHERE id = ?`,
-    [id]
-  );
-  res.json({
-    msg: `${id}번 할일이 삭제되었습니다.`,
-  });
 });
 
 app.listen(port, () => {
